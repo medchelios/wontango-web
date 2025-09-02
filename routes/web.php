a93b7d7b9753cdec96d7739d67e3d68a1191ae43
Mohamed TOURE <toure1206@gmail.com>
2025-09-02 10:30:58 -0400

    Add home route to web.php for Inertia Welcome page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use App\Http\Controllers\LeadController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
 
 Route::post('/leads', [LeadController::class, 'store'])
     ->middleware(['throttle:5,1'])
